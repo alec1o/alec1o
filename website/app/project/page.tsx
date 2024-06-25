67c442a33ec9cb396a857e396a91abbd820f0467
Alecio Furanze (Ale) <alecio@kezero.com>
2024-06-25 18:56:14 +0200

    add resume and project page content

--- website/app/project/page.tsx
+++ website/app/project/page.tsx
@@ -1,3 +1,11 @@
+import style from "./page.module.css";
+
 export default function Project() {
-    return <>TODO: Project</>;
+    return (
+        <>
+            <h1 className={style.title}>Alecio's project</h1>
+            <p className={style.warning}>“Curiosity killed the cat”</p>
+            <p className={style.info}>{"When your brain's RAM is full, and the cat just adds to the chaos."}</p>
+        </>
+    );
 }
--- website/app/resume/page.tsx
+++ website/app/resume/page.tsx
@@ -1,3 +1,13 @@
+import style from "./page.module.css";
+
 export default function Resume() {
-    return <>TODO: Resume</>
+    return (
+        <>
+            <h1 className={style.title}>
+                Alecio's resume <sup> (CV)</sup>
+            </h1>
+            <p className={style.warning}>“Curiosity killed the cat”</p>
+            <p className={style.info}>{"When your brain's RAM is full, and the cat just adds to the chaos."}</p>
+        </>
+    );
 }
